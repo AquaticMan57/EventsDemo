51e9d6d92b56c1db771e80651a1cd4656e4114fd
erayyaman5759@gmail.com <erayyaman5759@gmail.com>
2021-08-02 14:17:01 +0300

    Add project files.

---
+++ EventsDemo/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsDemo
+{
+    public delegate void StockControl();
+    class Product
+    {
+        private int _stock;
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+
+        public event StockControl StockControlEvent;
+        public int Stock { 
+            get 
+            {
+                return _stock; 
+            } 
+            set 
+            {
+                _stock = value; 
+                if (value <=15 && StockControlEvent != null) 
+                { 
+                    StockControlEvent(); 
+                } 
+
+            } 
+        }
+        public string ProductName { get; set; }
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            if (Stock <0)
+            {
+                Stock = 0;
+
+            }
+            Console.WriteLine("Stock Amount :{0}", Stock);
+        }
+
+    }
+}
---
+++ EventsDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EventsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product harddisc = new Product(50);
+            harddisc.ProductName = "Hard Disk";
+
+            Product gsm = new Product(50);
+            gsm.ProductName = "GSM";
+            gsm.StockControlEvent += Gsm_StockControlEvent;
+            for (int i = 0; i < 6; i++)
+            {
+                gsm.Sell(10);
+                harddisc.Sell(9);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            
+            
+            Console.WriteLine("GSM is about to finish");
+        }
+    }
+    
+}
